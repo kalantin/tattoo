ada8dcfc24a22c945cfa6c15d75a94942795e9c4
kalantin <kalantin@gmail.com>
2021-12-17 20:35:27 +0100

    created section about us and cards

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
+const cardArtist = document.querySelector('.aboutus__img');
+const cardArtistRight = document.querySelector('.aboutus__img--right');
+const cardText = document.querySelector('.aboutus__text');
+const cardTextRight = document.querySelector('.aboutus__text--right');
 
 
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
-
+}
+const cardTextHide = () => {
+    cardText.classList.toggle('aboutus__text-active');
+    cardArtist.classList.toggle('aboutus__img-active');
+    cardTextRight.classList.remove('aboutus__text-active--right');
+    cardArtistRight.classList.remove('aboutus__img-active');
+}
+const cardTextHideRight = () =>{
+    cardTextRight.classList.toggle('aboutus__text-active--right');
+    cardArtistRight.classList.toggle('aboutus__img-active');
+    cardText.classList.remove('aboutus__text-active');
+    cardArtist.classList.remove('aboutus__img-active');
 }
 
 
 
-
 navBtn.addEventListener('click', handleNav);
+cardArtist.addEventListener('click', cardTextHide);
+cardArtistRight.addEventListener('click', cardTextHideRight);
