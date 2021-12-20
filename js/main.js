008a53c4bc1620398e9034c67f18cd285ac543c2
kalantin <kalantin@gmail.com>
2021-12-20 22:38:41 +0100

    created next section, before

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
+const head = document.querySelector('.before__box-head');
+const headingBox = document.querySelector('.before__box');
+const imgUp = document.querySelector('.before__box-img--up');
+const imgDown = document.querySelector('.before__box-img--down');
 
 
 
@@ -9,7 +13,13 @@ const handleNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
 }
+function show (){
+     imgUp.classList.toggle('before__box-img--up-hide');
+     imgDown.classList.toggle('before__box-img--down-show');
+    headingBox.classList.toggle('before__box-show');
+}
 
 
 
 navBtn.addEventListener('click', handleNav);
+head.addEventListener('click', show);
