0da6b5231794456384dd9299c8b73892639c2fa6
kalantin <kalantin@gmail.com>
2021-12-24 20:30:48 +0100

    added responsive style

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ const head = document.querySelector('.before__box-head');
 const headingBox = document.querySelector('.before__box');
 const imgUp = document.querySelector('.before__box-img--up');
 const imgDown = document.querySelector('.before__box-img--down');
+const footerYear = document.querySelector ('.footer__year');
 
-
-
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
@@ -20,6 +23,6 @@ function show (){
 }
 
 
-
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
 head.addEventListener('click', show);
